44faab1330e045ee7e6265def7d17c0652e14ab0
Daria <dsnaydina@gmail.com>
2020-09-14 10:44:57 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int balance = 1000;
+        int topUp = 3000;
+        if (balance >= 1000) {
+            float bonus = topUp / 100;
+            System.out.println(bonus);
+
+
+        }
+    }
+}
